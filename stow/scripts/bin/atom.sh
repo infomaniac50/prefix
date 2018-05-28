27579effd2b26e7b752fc1938cd6069628664124
Derek Chafin <infomaniac50@gmail.com>
2018-05-27 22:56:20 -0500

    Provide Atom with it's own PHP build.
    Fix for https://github.com/Gert-dev/php-ide-serenata/issues/431 indexing stuck on everytime modified php file
    @Gert-dev says to disable xdebug for php-ide-serenata

--- stow/scripts/bin/atom.sh
+++ stow/scripts/bin/atom.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-exec /bin/bash -ic "loader rvm nvm node phpbrew composer && exec /usr/bin/atom $*"
+# To create php-atom, use phpbrew to build php but without the xdebug extension.
+# Then rsync .phpbrew/php/php-<version> to .phpbrew/php/php-atom.
+# Make sure Atom package configs also point to .phpbrew/php/php-atom.
+exec /bin/bash -ic "loader rvm nvm node phpbrew composer && phpbrew use php-atom && exec /usr/bin/atom $*"
