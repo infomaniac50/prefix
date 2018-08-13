fcc8e075f7a4ac9e34cefc2e97d69e7dda7790ae
Derek Chafin <infomaniac50@gmail.com>
2018-08-13 16:16:42 -0500

    Remove atom.sh

--- stow/scripts/bin/atom.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# To create php-atom, use phpbrew to build php but without the xdebug extension.
-# Then rsync .phpbrew/php/php-<version> to .phpbrew/php/php-atom.
-# Make sure Atom package configs also point to .phpbrew/php/php-atom.
-exec /bin/bash -ic "loader rvm nvm node phpbrew composer && phpbrew use php-atom && exec /usr/bin/atom $*"
